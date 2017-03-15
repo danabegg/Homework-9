c98bd0cf9f7d064863cfcaeb283a4f1588a009e3
danabegg <dana.begg@gmail.com>
2017-03-15 16:20:52 -0700

    made database

--- index.js
+++ index.js
@@ -54,15 +54,14 @@ var locations = [
 
 
 ///////////////////////
-
 //Test for browser compatibility
 if (window.openDatabase) {
     //Create the database the parameters are 1. the database name 2.version number 3. a description 4. the size of the database (in bytes) 1024 x 1024 = 1MB
-    var mydb = openDatabase("cars_db", "0.1", "A Database of Cars I Like", 1024 * 1024);
+    var mydb = openDatabase("locations_db", "0.1", "A Database of Locations", 1024 * 1024);
 
     //create the cars table using SQL for the database using a transaction
     mydb.transaction(function(t) {
-        t.executeSql("CREATE TABLE IF NOT EXISTS cars (id INTEGER PRIMARY KEY ASC, make TEXT, model TEXT)");
+        t.executeSql("CREATE TABLE IF NOT EXISTS locations (id INTEGER PRIMARY KEY ASC, name TEXT, lat TEXT, long TEXT)");
     });
 
 
@@ -73,13 +72,13 @@ if (window.openDatabase) {
 
 //function to output the list of cars in the database
 
-function updateCarList(transaction, results) {
-		console.log(transaction);
+function updatelocationlist(transaction, results) {
+	console.log(transaction);
     console.log(results);
     //initialise the listitems variable
     var listitems = "";
     //get the car list holder ul
-    var listholder = document.getElementById("carlist");
+    var listholder = document.getElementById("locationlist");
 
     //clear cars list ul
     listholder.innerHTML = "";
@@ -90,19 +89,19 @@ function updateCarList(transaction, results) {
         //Get the current row
         var row = results.rows.item(i);
 
-        listholder.innerHTML += "<li>" + row.make + " - " + row.model + " (<a href='javascript:void(0);' onclick='deleteCar(" + row.id + ");'>Delete Location</a>)";
+        listholder.innerHTML += "<li>" + row.name + " - " + row.lat + " - " + row.long + " (<a href='javascript:void(0);' onclick='deletelocation(" + row.id + ");'>Delete Location</a>)";
     }
 
 }
 
 //function to get the list of cars from the database
 
-function outputCars() {
+function outputlocations() {
     //check to ensure the mydb object has been created
     if (mydb) {
         //Get all the cars from the database with a select statement, set outputCarList as the callback function for the executeSql command
         mydb.transaction(function(t) {
-            t.executeSql("SELECT * FROM cars", [], updateCarList);
+            t.executeSql("SELECT * FROM locations", [], updatelocationlist);
         });
     } else {
         alert("db not found, your browser does not support web sql!");
@@ -111,23 +110,23 @@ function outputCars() {
 
 //function to add the car to the database
 
-function addCar() {
+function addlocation() {
     //check to ensure the mydb object has been created
     if (mydb) {
         //get the values of the make and model text inputs
-        var custom = document.getElementById("custom").value;
-        var make = document.getElementById("carmake").value;
-        var model = document.getElementById("carmodel").value;
+        var name = document.getElementById("name").value;
+        var lat = document.getElementById("lat").value;
+        var long = document.getElementById("long").value;
 
         //Test to ensure that the user has entered both a make and model
-        if (make !== "" && model !== "") {
+        if (name !== "" && lat !== "" && long !== "") {
             //Insert the user entered details into the cars table, note the use of the ? placeholder, these will replaced by the data passed in as an array as the second parameter
             mydb.transaction(function(t) {
-                t.executeSql("INSERT INTO cars (make, model) VALUES (?, ?)", [make, model]);
-                outputCars();
+                t.executeSql("INSERT INTO locations (name, lat, long) VALUES (?, ?, ?)", [name, lat, long]);
+                outputlocations();
             });
         } else {
-            alert("You must enter a make and model!");
+            alert("You must enter a name, latitude, and longitude!");
         }
     } else {
         alert("db not found, your browser does not support web sql!");
@@ -137,16 +136,16 @@ function addCar() {
 
 //function to remove a car from the database, passed the row id as it's only parameter
 
-function deleteCar(id) {
+function deletelocation(id) {
     //check to ensure the mydb object has been created
     if (mydb) {
         //Get all the cars from the database with a select statement, set outputCarList as the callback function for the executeSql command
         mydb.transaction(function(t) {
-            t.executeSql("DELETE FROM cars WHERE id=?", [id], outputCars);
+            t.executeSql("DELETE FROM locations WHERE id=?", [id], outputLocations);
         });
     } else {
         alert("db not found, your browser does not support web sql!");
     }
 }
 
-outputCars();
+outputlocations();
